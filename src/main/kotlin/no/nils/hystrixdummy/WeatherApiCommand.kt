2433a2e747ffeabbc910e6f15a7e6a4e541cca30
Nils Larsgård <nilsmagnus@gmail.com>
2017-10-03 11:03:55 +0200

    Add random sleep in run to get some variance in latency

--- src/main/kotlin/no/nils/hystrixdummy/WeatherApiCommand.kt
+++ src/main/kotlin/no/nils/hystrixdummy/WeatherApiCommand.kt
@@ -2,10 +2,12 @@ package no.nils.hystrixdummy
 
 import com.netflix.hystrix.HystrixCommand
 import com.netflix.hystrix.HystrixCommandGroupKey
+import kotlinx.coroutines.experimental.delay
 
 class WeatherApiCommand<T>(val operation: () -> T) : HystrixCommand<T>(HystrixCommandGroupKey.Factory.asKey(WeatherApiCommand::class.simpleName)) {
 
     override fun run(): T {
+        Thread.sleep((Math.random()*500 + 10).toLong())
         return operation()
     }
 
